90756f763a8e09b5868903e6c7904187d6e2c7c6
fuxialexander <fuxialexander@gmail.com>
2019-04-27 12:58:38 +0000

    remove xclip

--- packages.el
+++ packages.el
@@ -8,6 +8,7 @@
 (package! ob-mongo :disable t)
 (package! ob-sql-mode :disable t)
 (package! ob-translate :disable t)
+(package! xclip :disable t)
 ;; * UI
 (package! prettify-utils :recipe (:fetcher github :repo "Ilazki/prettify-utils.el" :files ("*")))
 (package! shr-tag-pre-highlight)
